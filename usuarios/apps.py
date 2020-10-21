ef0a65d11168f92e41346974afd80da93abc5655
Victoria <zappettiniv@gmail.com>
2020-10-21 17:34:44 -0300

    probando

--- usuarios/apps.py
+++ usuarios/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class UsuariosConfig(AppConfig):
     name = 'usuarios'
+
+
+
+#prueba
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
+ from django.http import HttpResponse, JsonResponse
 # Create your views here.
 def home(request):
     return render(request, 'home.html', context={})
 
+
+
+def holaMundo(request):
+    return HttpResponse(“hola mundo”)
